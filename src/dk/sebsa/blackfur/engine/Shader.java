15da46911a447608791b13933e79a9466c331c0c
Sebastian Snoer <seb.snoer@gmail.com>
2020-07-30 09:33:17 +0200

    Trying to fix shader

--- src/dk/sebsa/blackfur/engine/Shader.java
+++ src/dk/sebsa/blackfur/engine/Shader.java
@@ -1,5 +1,7 @@
 package dk.sebsa.blackfur.engine;
 
+import static org.lwjgl.opengl.GL20.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,71 +11,72 @@ import java.util.List;
 
 import org.lwjgl.BufferUtils;
 
-import org.lwjgl.opengl.GL20;
-
 import dk.sebsa.blackfur.math.Color;
 import dk.sebsa.blackfur.math.Matrix4x4;
 import dk.sebsa.blackfur.math.Vector2f;
 
 public class Shader {
 	public String name;
-	private static List<Shader> shaders = new ArrayList<Shader>();
 	
-	private static int i;
 	private int program;
 	private int vs;
 	private int fs;
+	
+	//Static variables to handle all of our shaders
+	private static List<Shader> shaders = new ArrayList<Shader>();
+	private static int i;
+	
 	private static Color boundColor = Color.transparent();
 	
 	public Shader(String fileName) throws IOException {
 		name = fileName;
 		
 		// Program
-		program = GL20.glCreateProgram();
+		program = glCreateProgram();
 		if (program == 0)
             throw new IllegalStateException("Could not create Shader");
         
 		String[] shader = createShader(fileName);
 		
-		// Vertex
-		vs = GL20.glCreateShader(GL20.GL_VERTEX_SHADER);
+		// Creates and stores the vertex shader. Then compiles it and checks for errors
+		vs = glCreateShader(GL_VERTEX_SHADER);
 		if (vs == 0)
             throw new IllegalStateException("Error creating shader. Type: Vertex Shader");
         
-		GL20.glShaderSource(vs, shader[0]);
-		GL20.glCompileShader(vs);
+		glShaderSource(vs, shader[0]);
+		glCompileShader(vs);
 		
-		if(GL20.glGetShaderi(vs, GL20.GL_COMPILE_STATUS) != 1) 
-			throw new IOException("Error compiling Shader code: " + GL20.glGetShaderInfoLog(vs, 1024));
+		if(glGetShaderi(vs, GL_COMPILE_STATUS) != 1) 
+			throw new IOException("Error compiling Shader code: " + glGetShaderInfoLog(vs, 1024));
 		
 		// Fragment
-		fs = GL20.glCreateShader(GL20.GL_FRAGMENT_SHADER);
-		if (vs == 0)
+		fs = glCreateShader(GL_FRAGMENT_SHADER);
+		if (fs == 0)
             throw new IllegalStateException("Error creating shader. Type: Fragment Shader");
         
-		GL20.glShaderSource(fs, shader[1]);
-		GL20.glCompileShader(fs);			
-		if(GL20.glGetShaderi(fs, GL20.GL_COMPILE_STATUS) != 1) 
-			throw new IOException("Error compiling Shader code: " + GL20.glGetShaderInfoLog(fs, 1024));
+		glShaderSource(fs, shader[1]);
+		glCompileShader(fs);			
+		if(glGetShaderi(fs, GL_COMPILE_STATUS) != 1) 
+			throw new IOException("Error compiling Shader code: " + glGetShaderInfoLog(fs, 1024));
 		
 		// Attach shaders to program
-		GL20.glAttachShader(program, vs);
-		GL20.glAttachShader(program, fs);
+		glAttachShader(program, vs);
+		glAttachShader(program, fs);
 		
 		// Bind attrib
-		GL20.glBindAttribLocation(program, 0, "verticies");
-		GL20.glBindAttribLocation(program, 1, "uv");
+		glBindAttribLocation(program, 0, "verticies");
+		glBindAttribLocation(program, 1, "uv");
 		
 		// Link, validate and check program
-		GL20.glLinkProgram(program);
+		glLinkProgram(program);
 		
-		if (GL20.glGetProgrami(program, GL20.GL_LINK_STATUS) == 0) {
-            throw new UnknownError("Error linking Shader code: " + GL20.glGetProgramInfoLog(program, 1024));
+		if (glGetProgrami(program, GL_LINK_STATUS) == 0) {
+            throw new UnknownError("Error linking Shader code: " + glGetProgramInfoLog(program, 1024));
         }
 		
-		GL20.glValidateProgram(program);
-		if (GL20.glGetProgrami(program, GL20.GL_VALIDATE_STATUS) == 0) {
-        	System.err.println("Warning validating Shader code: " + GL20.glGetProgramInfoLog(program, 1024));
+		glValidateProgram(program);
+		if (glGetProgrami(program, GL_VALIDATE_STATUS) == 0) {
+        	System.err.println("Warning validating Shader code: " + glGetProgramInfoLog(program, 1024));
         }
 		
 		shaders.add(this);
@@ -97,13 +100,13 @@ public class Shader {
 	}
 	
 	public void setUniform(String name, Color c) {
-		int location = GL20.glGetUniformLocation(program, name);
-		if(location != -1) GL20.glUniform4f(location, c.r, c.g, c.b, c.a);
+		int location = glGetUniformLocation(program, name);
+		if(location != -1) glUniform4f(location, c.r, c.g, c.b, c.a);
 	}
 	
 	public void setUniform(String name, float x, float y) {
-		int location = GL20.glGetUniformLocation(program, name);
-		if(location != -1) GL20.glUniform2f(location, x, y);
+		int location = glGetUniformLocation(program, name);
+		if(location != -1) glUniform2f(location, x, y);
 	}
 	
 	public void setUniform(String name, Vector2f v) {
@@ -111,17 +114,17 @@ public class Shader {
 	}
 	
 	public void setUniform(String name, float x, float y, float z, float w) {
-		int location = GL20.glGetUniformLocation(program, name);
-		if(location != -1) GL20.glUniform4f(location, x, y, z, w);
+		int location = glGetUniformLocation(program, name);
+		if(location != -1) glUniform4f(location, x, y, z, w);
 	}
 	
 	public void setUniform(String name, Matrix4x4 matrix) {
-		int location = GL20.glGetUniformLocation(program, name);
+		int location = glGetUniformLocation(program, name);
 		
 		FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
 		matrix.getBuffer(buffer);
 		
-		if(location != -1) GL20.glUniformMatrix4fv(location, false, buffer);
+		if(location != -1) glUniformMatrix4fv(location, false, buffer);
 		buffer.flip();
 	}
 	
@@ -133,17 +136,17 @@ public class Shader {
 	}
 	
 	public void bind() {
-		GL20.glUseProgram(program);
+		glUseProgram(program);
 	}
 	
 	public void unbind() {
-		GL20.glUseProgram(0);
+		glUseProgram(0);
 	}
 	
 	public void cleanup() {
         unbind();
         if (program != 0) {
-            GL20.glDeleteProgram(program);
+            glDeleteProgram(program);
         }
     }
 
